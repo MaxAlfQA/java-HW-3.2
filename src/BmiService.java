b52610732ecc4bd9c82d097eeccc02f16c8e3ebb
max alf <fr4666@yandex.ru>
2025-07-31 13:46:50 +0300

    good varibals

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
 public class BmiService {
-    public int calculate(double a, int b) {
+    public int calculate(double height, int weight) {
         int result;
-        result = (int) (b / (a * a));
+        result = (int) (weight / (height * height));
         return result;
     }
 }
--- src/main.java
+++ src/main.java
@@ -2,9 +2,9 @@ public class main {
 
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double x = 1.87; // рост в метрах
-        int y = 98;  // масса в киллограмах
-        int bmi = service.calculate(x, y);
+        double height = 1.87; // рост в метрах
+        int weight = 98;  // масса в киллограмах
+        int bmi = service.calculate(height, weight);
         System.out.println(bmi);
     }
 }
